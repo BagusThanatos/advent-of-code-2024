11b26f735c672f5c4b17bd0d559d3a51d7b6d895
Bagus Nurtomo <bagus.nurtomo@pm.me>
2024-12-02 13:40:35 +0700

    Day 2 part 2 done
    Signed-off-by: Bagus Nurtomo <bagus.nurtomo@pm.me>

--- 2/main_part2.go
+++ 2/main_part2.go
@@ -7,8 +7,38 @@ import (
   "fmt"
   "io"
   "strings"
-  // "sort"
 )
+
+
+func checkValid(line []int64) bool {
+  valid := true
+  ascending := true
+
+  num1 := line[0]
+  num2 := line[1]
+  if num2 < num1 {
+    ascending = false
+  }
+
+  if ascending {
+    for i:=1; valid && i<len(line); i++ {
+      num1 := line[i-1]
+      num2 := line[i]
+      if num1>=num2 || num2-num1 > 3{
+        valid = false
+      }
+    }
+  } else {
+    for i:=1; valid && i<len(line); i++ {
+      num1 := line[i-1]
+      num2 := line[i]
+      if num1<=num2 || num1-num2 > 3 {
+        valid = false
+      }
+    }
+  }
+  return valid && num1 != num2
+}
 
 
 func main() {
@@ -24,97 +54,29 @@ func main() {
   }
 
   lines := strings.Split(string(bytes), "\n")
-  reports := make([][]int64, len(lines-1))
+  reports := make([][]int64, len(lines)-1)
   for i, line := range lines[:len(lines)-1] {
     nums := strings.Split(line, " ")
     levels := make([]int64, len(nums))
     for i:=0;i<len(nums);i++{
-      levels[i] = strconv.ParseInt(nums[i], 10, 64)
+      levels[i], _ = strconv.ParseInt(nums[i], 10, 64)
     }
     reports[i] = levels
   }
 
   validCount := 0
   for _, line := range reports {
-    valid := true
-    ascending := true
-
-    num1, _ := line[0]
-    num2, _ := line[1]
-    if num2 < num1 {
-      ascending = false
-    }
-
-    if ascending {
-      for i:=1; valid && i<len(nums); i++ {
-        num1, _ := strconv.ParseInt(nums[i-1], 10, 64)
-        num2, _ := strconv.ParseInt(nums[i], 10, 64)
-        if num1>=num2 || num2-num1 > 3{
-          valid = false
-        }
-      }
-    } else {
-      for i:=1; valid && i<len(nums); i++ {
-        num1, _ := strconv.ParseInt(nums[i-1], 10, 64)
-        num2, _ := strconv.ParseInt(nums[i], 10, 64)
-        if num1<=num2 || num1-num2 > 3 {
-          valid = false
-        }
-      }
-    }
+    valid := checkValid(line)
     if !valid {
-      // try by removing one element
-    for _, line := range lines[:len(lines)-1] {
-      valid := true
-      ascending := true
-      once := true
-      nums := strings.Split(line, " ")
-
-      num1, _ := strconv.ParseInt(nums[0], 10, 64)
-      num2, _ := strconv.ParseInt(nums[1], 10, 64)
-      if num2 < num1 {
-        ascending = false
-      }
-
-      if ascending {
-        for i:=1; valid && i<len(nums); i++ {
-          num1, _ := strconv.ParseInt(nums[i-1], 10, 64)
-          num2, _ := strconv.ParseInt(nums[i], 10, 64)
-          if num1>=num2 || num2-num1 > 3{
-            if i+1 < len(nums) {
-              num3, _ := strconv.ParseInt(nums[i+1], 10, 64)
-              if !once || (num1>=num3 || num3-num1 > 3){
-                valid = false
-              } else {
-                i++
-                once = false
-              }
-            } else {
-              valid = false
-            }
-          }
-        }
-      } else {
-        for i:=1; valid && i<len(nums); i++ {
-          num1, _ := strconv.ParseInt(nums[i-1], 10, 64)
-          num2, _ := strconv.ParseInt(nums[i], 10, 64)
-          if num1<=num2 || num1-num2 > 3 {
-            if i+1 < len(nums) {
-              num3, _ := strconv.ParseInt(nums[i+1], 10, 64)
-              if !once || (num1>=num3 || num3-num1 > 3) {
-                valid = false
-              } else {
-                i++
-                once = false
-              }
-            } else {
-              valid = false
-            }
-          }
-        }
+      // try remove 1 element
+      for i:=0; !valid && i<len(line); i++ {
+        var temp []int64
+        temp = append(temp, line[:i]...)
+        temp = append(temp, line[i+1:]...)
+        valid = checkValid(temp)
       }
     }
-    if valid && num1 != num2 {
+    if valid{
       fmt.Println(line)
       validCount++
     }
