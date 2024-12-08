1af5c1a23575394879988cf63f15566d7bc82a25
Bagus Nurtomo <bagus.nurtomo@pm.me>
2024-12-08 12:27:53 +0700

    day 8 done
    Signed-off-by: Bagus Nurtomo <bagus.nurtomo@pm.me>

---
+++ 8/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "io"
+  "strings"
+)
+
+func main() {
+  file, err := os.Open("input.txt")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  bytes, err := io.ReadAll(file)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  lines := strings.Split(string(bytes), "\n")
+  lines = lines[:len(lines)-1]
+  result := make([][]int64, len(lines))
+  for i:=0;i<len(result);i++{
+    result[i] = make([]int64, len(lines[0]))
+  }
+  for i:=0;i<len(lines);i++{
+    for j:=0;j<len(lines[i]);j++{
+      currentNode := lines[i][j]
+      if currentNode == '.' {
+        continue
+      }
+      // look for corresponding nodes
+      for y:=0;y<len(lines);y++{
+        for x:=0;x<len(lines);x++{
+          if y==i && x==j {
+            continue
+          }
+          if lines[y][x] == currentNode {
+            newY := y + (y-i)
+            newX := x + (x-j)
+            if newY >=0 && newY < len(lines) && newX >=0 && newX < len(lines[0]) {
+              result[newY][newX] = 1
+            }
+          }
+        }
+      }
+    }
+  }
+
+  sum := int64(0)
+  for i:=0;i<len(result);i++{
+      fmt.Println(result[i])
+    for j:=0;j<len(result[i]);j++{
+      sum+=result[i][j]
+    }
+  }
+  fmt.Println(sum)
+}
