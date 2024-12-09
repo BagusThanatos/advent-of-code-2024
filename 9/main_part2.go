5f020ac5f010df08375bffe8e0e93873523aa69b
Bagus Nurtomo <bagus.nurtomo@pm.me>
2024-12-09 15:49:22 +0700

    Day 9 part 2 done
    Signed-off-by: Bagus Nurtomo <bagus.nurtomo@pm.me>

--- 9/main_part2.go
+++ 9/main_part2.go
@@ -22,12 +22,12 @@ func main() {
   lines := strings.Split(string(bytes), "\n")
   input := lines[0]
 
-  harddisk := make([]int64, 1024*1024) // 1KB of harddisk
+  harddisk := make([]int64, 94521) // Length was from it's maximum elements
 
   EMPTYBLOCK := int64(-1)
   currentFileID := int64(0)
   currentHDIndex := 0
-
+  fmt.Println(len(lines[0]))
   // generate block rules first
   fmt.Println("generate block rules")
   for i, v := range input {
@@ -43,13 +43,12 @@ func main() {
       currentHDIndex++
     }
   }
-  fmt.Println(harddisk[:100])
+  PrintHDD(harddisk)
   fmt.Println("move blocks")
-  fmt.Println(currentHDIndex, harddisk[currentHDIndex])
   currentHDIndex--
+  fmt.Println(currentHDIndex, harddisk[currentHDIndex])
   for currentHDIndex >=0{
     // determine file length
-    // fmt.Println("determine file length")
     length := 0
     currentFile := harddisk[currentHDIndex]
     for currentHDIndex >= 0 && harddisk[currentHDIndex] == currentFile{
@@ -59,7 +58,8 @@ func main() {
     // look for longest empty block that can fit from leftmost
     emptyBlockLoc := 0
     currentEmptyBlockLength := 0
-    for emptyBlockLoc < currentHDIndex && currentEmptyBlockLength < length {
+    // NOTE: was stuck here due to `< currentHDIndex` not covering cases such `-1 -1 -1 1 1 1`
+    for emptyBlockLoc <= currentHDIndex && currentEmptyBlockLength < length {
       if harddisk[emptyBlockLoc] == EMPTYBLOCK {
         currentEmptyBlockLength++
       } else {
@@ -71,12 +71,10 @@ func main() {
 
     // move blocks
     if currentEmptyBlockLength == length {
-      // fmt.Println("move blocks for ", currentFile)
       for i:=1;i<=length;i++{
         harddisk[emptyBlockLoc-i] = harddisk[currentHDIndex+i]
         harddisk[currentHDIndex+i] = EMPTYBLOCK
       }
-      // fmt.Println(harddisk[:50])
     }
     for ;currentHDIndex>=0 && harddisk[currentHDIndex] == EMPTYBLOCK; currentHDIndex--{}
   }
@@ -87,29 +85,11 @@ func main() {
       sum += (int64(i) * harddisk[i])
     }
   }
-  // try to validate result
-  // valid := map[int64][]int{}
-  // curr := harddisk[0]
-  // valid[curr] = []int{0}
-  // for i := 1;i<len(harddisk); i++{
-  //   if harddisk[i] > 0 && harddisk[i] != curr {
-  //     if v, ok := valid[curr]; ok {
-  //       valid[curr] = append(v, i)
-  //     } else {
-  //       valid[curr] = []int{i}
-  //     }
-  //     curr = harddisk[i]
-  //   }
-    // if harddisk[i] == 0 {
-    //   fmt.Println(i)
-    // }
-  // }
-  // for k, v := range valid {
-  //   if len(v) > 1 {
-  //     fmt.Println("occur twice: ", k, v)
-  //   }
-  // }
-  // fmt.Println(valid)
-  fmt.Println(harddisk[:100])
+  PrintHDD(harddisk)
   fmt.Println(sum)
+}
+
+func PrintHDD(harddisk []int64){
+  fmt.Println(harddisk[:100])
+  fmt.Println(harddisk[len(harddisk)-50000:len(harddisk)-49900])
 }
